1ade6fe0603bb5d7fb8e346050a8a2654c4a7683
Federico Brigante <bfred-it@users.noreply.github.com>
2019-04-09 13:38:46 +0700

    await promise

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ import withBrowser from './tests/helpers/with-browser';
 import indentTextarea from '.';
 
 test('add single tab without selection', withBrowser, async t => {
+console.log(t.context.field)
 	t.context.field.value = 'hello';
 	indentTextarea(t.context.field);
 	t.is(t.context.field.value, '\thello');
--- tests/helpers/with-browser.js
+++ tests/helpers/with-browser.js
@@ -5,7 +5,8 @@ module.exports = async function withBrowser(t, run) {
 	const page = await browser.newPage();
 	await page.setContent('<textarea>');
 
-	t.context.field = page.$('textarea');
+	t.context.field = await page.$('textarea');
+	console.log(t.context.field)
 
 	try {
 		await run(t, page);
