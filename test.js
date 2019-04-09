03cbf14dc2a96398466e7df68b16f72d1891198d
Federico Brigante <bfred-it@users.noreply.github.com>
2019-04-09 10:48:06 +0700

    lint

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import withBrowser from './tests/helpers/with-browser';
 import indentTextarea from '.';
 
-test('add single tab without selection', withBrowser, async (t, page) => {
+test('add single tab without selection', withBrowser, async t => {
 	t.context.field.value = 'hello';
 	indentTextarea(t.context.field);
 	t.is(t.context.field.value, '\thello');
