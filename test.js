b8698af6ab4310d828617c7dafb0611f2c54a832
Federico Brigante <bfred-it@users.noreply.github.com>
2019-04-12 16:12:25 +0700

    add tests

--- test.js
+++ test.js
@@ -1,9 +1,55 @@
-import test from 'ava';
-import withBrowser from './tests/helpers/with-browser';
-import indentTextarea from '.';
+const test = require('tape');
+const indent = require('.');
 
-test('add single tab without selection', withBrowser, async (t, field) => {
-	field.value = 'hello';
-	indentTextarea(field);
-	t.is(field.value, '\thello');
+const getField = (value = '', start, end) => {
+	const field = document.createElement('textarea');
+	field.value = value;
+	document.body.append(field);
+	if (end !== undefined) {
+		field.selectionStart = start;
+		field.selectionEnd = end;
+	}
+
+	return field;
+};
+
+test('insert tab in empty field', t => {
+	const textarea = getField();
+	t.equal(textarea.value, '');
+	indent(textarea);
+	t.equal(textarea.value, '\t');
+	indent(textarea);
+	t.equal(textarea.value, '\t\t');
+	t.equal(textarea.selectionStart, 2);
+	t.equal(textarea.selectionEnd, 2);
+	t.end();
+});
+
+
+test('insert tab in filled field', t => {
+	const textarea = getField('hello');
+	t.equal(textarea.value, 'hello');
+	indent(textarea);
+	t.equal(textarea.value, 'hello\t');
+	t.equal(textarea.selectionStart, 7);
+	t.equal(textarea.selectionEnd, 7);
+	t.end();
+});
+
+test('insert tab and replace selection', t => {
+	const textarea = getField('hello', 0, 5);
+	indent(textarea);
+	t.equal(textarea.value, '\to');
+	t.equal(textarea.selectionStart, 2);
+	t.equal(textarea.selectionEnd, 2);
+	t.end();
+});
+
+test('insert tab on every selected line', t => {
+	const textarea = getField('a\nb', 0, 3);
+	indent(textarea);
+	t.equal(textarea.value, '\ta\n\tb');
+	t.equal(textarea.selectionStart, 2);
+	t.equal(textarea.selectionEnd, 2);
+	t.end();
 });
